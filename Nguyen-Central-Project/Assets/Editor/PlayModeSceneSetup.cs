6ba13957ddbcadb658102f8d79c6647e07a1d59d
San Diego Mesa College <sdmesasoftware@sdccd.edu>
2026-09-21 20:10:19 -0700

    Improved systems

---
+++ Nguyen-Central-Project/Assets/Editor/PlayModeSceneSetup.cs
@@ -0,0 +1,21 @@
+/*using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+
+public class PlayModeSceneSetup
+{
+        static PlayModeSceneSetup()
+    {
+        EditorApplication.delayCall += OpenStartupScene;
+    }
+        private static void OpenStartupScene()
+    {
+        string scenePath = "Assets/_scenes/Sandbox 2.unity";
+
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene(scenePath);
+        }
+    }
+}*/
--- Nguyen-Central-Project/Assets/Module_03_Assignment/Scripts/Gameplay/PlayerJump.cs
+++ Nguyen-Central-Project/Assets/Module_03_Assignment/Scripts/Gameplay/PlayerJump.cs
@@ -4,4 +4,51 @@ public class PlayerJump : MonoBehaviour
 {
     [Header("Jump Settings")]
     public float jumpHeight = 8f;
+
+
+    [Header("Input")]
+    public InputActionReference jumpAction;
+
+    [Header("is on Ground")]
+
+    public Transform groundCheck;
+    public float groundDistance = 0.25f;
+    public LayerMask groundLayer;
+
+    [Header("Weight Setting")]
+    public PlayerGrab playerGrab;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+    public float jumpForce = 8f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        if (jumpAction.action.triggered && isGrounded)
+        {
+            Jump();
+        }
+    }
+       void Jump()
+    {
+        float finalJumpForce = jumpForce;
+
+        if (playerGrab.carriedWeight > 0)
+        {
+            finalJumpForce -= playerGrab.carriedWeight * 0.1f;
+        }
+   
+        finalJumpForce = Mathf.Max(finalJumpForce, 2f);
+
+        rb.AddForce(Vector3.up * finalJumpForce, ForceMode.Impulse);
+    }
+
 }
+
